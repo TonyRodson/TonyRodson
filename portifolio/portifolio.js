8cb6a6bab944729bd8ceff8c960cf2fe6161d498
TonyRodson <tonyrodson@gmail.com>
2024-07-23 18:12:56 -0300

    Add files via upload

---
+++ portifolio/portifolio.js
@@ -0,0 +1,12 @@
+function inst(){
+    location.href='https://www.instagram.com/tony_rodson/'
+}
+function what(){
+    location.href='https://wa.me/5589999931207'
+}
+function cor(){
+    document.body.classList.toggle('AAA')
+}
+function cont(){
+    window.location.href="https://mail.google.com/mail/?view=cm&fs=1&to=tonyrodson@gmail.com&su=Ola%20eu%20gostaria%20de%20saber%20mais%20sobre%20seu%20trabalho&body=Eu%20de%20gostaria%20de..."
+}
